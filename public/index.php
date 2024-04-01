50ffecc531a059bf58c621c4b5a2f33af4d19fe4
Abass Durodola <abass_gbolahan@yahoo.com>
2024-04-01 12:28:02 +0100

    router update

--- public/index.php
+++ public/index.php
@@ -4,7 +4,14 @@ require '../helpers.php';
 
 // loadView('home');
 
-$uri = $_SERVER['REQUEST_URI'];
-
 // inspectAndDie($uri);
- require basePath('router.php');
+require basePath('Router.php');
+
+$router = new Router();
+
+$routes = require basePath('routes.php');
+
+$uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+$router -> route($uri, $method);
